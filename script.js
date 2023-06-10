8cba3409b30d2fa87dc531c4c6805d42639d1d77
LukeBertus <127717618+LukeBertus@users.noreply.github.com>
2023-06-10 20:41:39 +0800

    testing on new machine

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 //#region querySelectors
+const page = document.querySelector(".page")
 const mainContainer = document.querySelector('.mainContainer')
-const imageContainer = document.querySelector('.imageContainer')
 
+
+let tracker = 1;
+let imageContainer = null;
 window.addEventListener('resize', e => {
-    let imageWidth = imageContainer.offsetWidth;
-    console.log(imageWidth)
-    if (imageWidth < 200) imageContainer.remove()
+    
+    let pageWidth = page.offsetWidth
+    console.log(tracker)
+    
+    if (pageWidth < 1200 && tracker === 1) {
+        if (imageContainer) {
+        imageContainer.remove();
+        imageContainer = null;
+        }
+        tracker = 0;
+    } 
+    if (pageWidth>=1200 && tracker === 0) {
+        tracker = 1;
+        imageContainer = document.createElement('div');
+        imageContainer.classList.add("imageContainer")
+        page.insertBefore(imageContainer, mainContainer);
+        
+    }
 })
-
+const onLoad = new Event("resize");
+window.dispatchEvent(onLoad);
 //#endregion
+h 
