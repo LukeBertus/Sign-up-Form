112f4b94004ebf87c0d093d5abeb3570ad494e0c
LukeBertus <127717618+LukeBertus@users.noreply.github.com>
2023-06-11 06:59:55 +0800

    Add adaptive page layout with js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 //#region querySelectors
 const page = document.querySelector(".page")
 const mainContainer = document.querySelector('.mainContainer')
+//#endregion
 
-
-let tracker = 1;
+//#region layout
+let tracker = 0;
 let imageContainer = null;
 window.addEventListener('resize', e => {
     
@@ -20,12 +21,13 @@ window.addEventListener('resize', e => {
     if (pageWidth>=1200 && tracker === 0) {
         tracker = 1;
         imageContainer = document.createElement('div');
-        imageContainer.classList.add("imageContainer")
+        imageContainer.classList.add("imageContainer");
         page.insertBefore(imageContainer, mainContainer);
-        
+        image = document.createElement('img');
+        image.classList.add("image");
+        imageContainer.appendChild(image);
     }
 })
 const onLoad = new Event("resize");
 window.dispatchEvent(onLoad);
 //#endregion
-h 
